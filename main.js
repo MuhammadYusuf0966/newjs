b2c65b73041f8f071b0e5753917f7cba97a04378
MuhammadYusuf0966 <yusufabdugofurov73@gmail.com>
2023-12-30 17:16:26 +0300

    bla bla

---
+++ main.js
@@ -0,0 +1,11 @@
+let kiritilganSon;
+
+
+const kiritganSon = prompt("Iltimos, bir son kiriting:");
+let juftBools = kiritganSon % 2 === 0;
+console.log(juftBools);
+
+
+// git remote add origin https://github.com/MuhammadYusuf0966/newjs.git
+// git branch -M main
+// git push -u origin main
